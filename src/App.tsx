d8104bf48e9f73d95ea8101cbcfe525d9c021b5a
Gabriel Souza <eugabrielf@gmail.com>
2022-12-20 13:32:36 -0300

    deixando layout dinamico

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import sidebarWeb from "./assets/img/Sidebar.png";
+import sidebarMobile from "./assets/img/Header_Background.png";
+
 
 function App() {
   return (
-    <div className="App">
-      Iniciando projeto
+    <div className="lotter-page-container">
+      <div className="lotter-sidebar">
+        <img src={sidebarMobile} alt={sidebarMobile} className="img-mobile" />
+        <img src={sidebarWeb} alt={sidebarWeb} height={"100%"}  className="img-web" />
+      </div>
+      <div className="lotter-container-numbers-games"></div>
     </div>
   );
 }
 
 export default App;
+
+//[] Desenvolver Layout
+//[] Sidebar dinamico cores
+//[] Responsivo
+//[] Teste unitarios
+//[] Refatora Codigo
